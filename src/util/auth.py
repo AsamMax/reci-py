a2a0755e054e1e4fa5a143a6f9a64252a3e26d00
Maximilian Asam <Maximilian.Asam1@HS-Augsburg.DE>
2023-07-14 14:01:06 +0200

    extended Sessions to 1 hour

--- src/util/auth.py
+++ src/util/auth.py
@@ -75,7 +75,7 @@ def create_access_token(
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.utcnow() + timedelta(hours=1)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
